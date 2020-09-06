2f251483fe676812354431d8f60efb9bedbdb831
st7712 <70844457+st7712@users.noreply.github.com>
2020-09-06 13:37:35 +0200

    Create coolgui.lua

---
+++ coolgui.lua
@@ -0,0 +1,224 @@
+--made by st#7712
+
+local coolgui = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local name = Instance.new("TextLabel")
+local by = Instance.new("TextLabel")
+local antilog = Instance.new("TextButton")
+local energize = Instance.new("TextButton")
+local infyield = Instance.new("TextButton")
+local invisfling = Instance.new("TextButton")
+local serverpinger = Instance.new("TextButton")
+local reviz = Instance.new("TextButton")
+local backdoorss = Instance.new("TextButton")
+local cmdx = Instance.new("TextButton")
+local trollgui = Instance.new("TextButton")
+local animsrblx = Instance.new("TextButton")
+local ragdollgui = Instance.new("TextButton")
+local animssynonly = Instance.new("TextButton")
+local noupdate = Instance.new("TextLabel")
+
+
+coolgui.Name = "cool gui"
+coolgui.Parent = game.CoreGui
+coolgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+main.Name = "main"
+main.Parent = coolgui
+main.BackgroundColor3 = Color3.fromRGB(61, 61, 61)
+main.BackgroundTransparency = 0.300
+main.BorderColor3 = Color3.fromRGB(0, 0, 0)
+main.Position = UDim2.new(0, 0, 0.422391862, 0)
+main.Size = UDim2.new(0, 435, 0, 227)
+main.Active = true
+main.Draggable = true
+
+name.Name = "name"
+name.Parent = main
+name.BackgroundColor3 = Color3.fromRGB(62, 62, 62)
+name.BackgroundTransparency = 0.200
+name.Position = UDim2.new(0.857471287, 0, 0, 0)
+name.Size = UDim2.new(0, 62, 0, 21)
+name.Font = Enum.Font.SourceSans
+name.Text = "cool gui"
+name.TextColor3 = Color3.fromRGB(0, 0, 0)
+name.TextSize = 14.000
+
+by.Name = "by"
+by.Parent = main
+by.BackgroundColor3 = Color3.fromRGB(61, 61, 61)
+by.BackgroundTransparency = 0.300
+by.BorderColor3 = Color3.fromRGB(61, 61, 61)
+by.Position = UDim2.new(0, 0, 0.0234375, 0)
+by.Size = UDim2.new(0, 83, 0, 15)
+by.Font = Enum.Font.SourceSans
+by.Text = "by st#7712"
+by.TextColor3 = Color3.fromRGB(0, 0, 0)
+by.TextSize = 14.000
+
+antilog.Name = "antilog"
+antilog.Parent = main
+antilog.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+antilog.Position = UDim2.new(0.0850574747, 0, 0.1328125, 0)
+antilog.Size = UDim2.new(0, 102, 0, 27)
+antilog.Font = Enum.Font.SourceSans
+antilog.Text = "Dot_mp4 antilogger"
+antilog.TextColor3 = Color3.fromRGB(0, 0, 0)
+antilog.TextSize = 14.000
+antilog.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/antisteal/antisteal-main/master/Anti-Steal.lua'),true))()
+end)
+
+energize.Name = "energize"
+energize.Parent = main
+energize.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+energize.Position = UDim2.new(0.0850574747, 0, 0.31640625, 0)
+energize.Size = UDim2.new(0, 102, 0, 27)
+energize.Font = Enum.Font.SourceSans
+energize.Text = "Energize animations"
+energize.TextColor3 = Color3.fromRGB(0, 0, 0)
+energize.TextSize = 14.000
+energize.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/RmD3qNp7", true))()
+end)
+
+infyield.Name = "inf yield"
+infyield.Parent = main
+infyield.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+infyield.Position = UDim2.new(0.675862074, 0, 0.1328125, 0)
+infyield.Size = UDim2.new(0, 102, 0, 27)
+infyield.Font = Enum.Font.SourceSans
+infyield.Text = "infinite yield admin"
+infyield.TextColor3 = Color3.fromRGB(0, 0, 0)
+infyield.TextSize = 14.000
+infyield.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
+end)
+
+invisfling.Name = "invis fling"
+invisfling.Parent = main
+invisfling.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+invisfling.Position = UDim2.new(0.381609201, 0, 0.1328125, 0)
+invisfling.Size = UDim2.new(0, 102, 0, 27)
+invisfling.Font = Enum.Font.SourceSans
+invisfling.Text = "Troll gui invis fling"
+invisfling.TextColor3 = Color3.fromRGB(0, 0, 0)
+invisfling.TextSize = 14.000
+invisfling.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/2kpbiZ5J", true))()
+end)
+
+serverpinger.Name = "server pinger"
+serverpinger.Parent = main
+serverpinger.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+serverpinger.Position = UDim2.new(0.0850574747, 0, 0.5078125, 0)
+serverpinger.Size = UDim2.new(0, 102, 0, 27)
+serverpinger.Font = Enum.Font.SourceSans
+serverpinger.Text = "Server pinger (lag)"
+serverpinger.TextColor3 = Color3.fromRGB(0, 0, 0)
+serverpinger.TextSize = 14.000
+serverpinger.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/CU7cXDk0", true))()
+end)
+
+reviz.Name = "reviz"
+reviz.Parent = main
+reviz.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+reviz.Position = UDim2.new(0.675862074, 0, 0.31640625, 0)
+reviz.Size = UDim2.new(0, 102, 0, 27)
+reviz.Font = Enum.Font.SourceSans
+reviz.Text = "REVIZ admin"
+reviz.TextColor3 = Color3.fromRGB(0, 0, 0)
+reviz.TextSize = 14.000
+reviz.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/dLpitEUG", true))()
+end)
+
+backdoorss.Name = "backdoor ss"
+backdoorss.Parent = main
+backdoorss.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+backdoorss.Position = UDim2.new(0.381609201, 0, 0.31640625, 0)
+backdoorss.Size = UDim2.new(0, 102, 0, 27)
+backdoorss.Font = Enum.Font.SourceSans
+backdoorss.Text = "Backdoor.exe"
+backdoorss.TextColor3 = Color3.fromRGB(0, 0, 0)
+backdoorss.TextSize = 14.000
+backdoorss.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/iK4oS/backdoor.exe/master/source.lua'),true))()
+end)
+
+cmdx.Name = "cmd x"
+cmdx.Parent = main
+cmdx.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+cmdx.Position = UDim2.new(0.675862074, 0, 0.5078125, 0)
+cmdx.Size = UDim2.new(0, 102, 0, 27)
+cmdx.Font = Enum.Font.SourceSans
+cmdx.Text = "CMD X admin"
+cmdx.TextColor3 = Color3.fromRGB(0, 0, 0)
+cmdx.TextSize = 14.000
+cmdx.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+end)
+
+trollgui.Name = "troll gui"
+trollgui.Parent = main
+trollgui.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+trollgui.Position = UDim2.new(0.381609201, 0, 0.5078125, 0)
+trollgui.Size = UDim2.new(0, 102, 0, 27)
+trollgui.Font = Enum.Font.SourceSans
+trollgui.Text = "Trolling GUI"
+trollgui.TextColor3 = Color3.fromRGB(0, 0, 0)
+trollgui.TextSize = 14.000
+trollgui.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/RNWgaqCE", true))()
+end)
+
+animsrblx.Name = "anims rblx"
+animsrblx.Parent = main
+animsrblx.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+animsrblx.Position = UDim2.new(0.675862074, 0, 0.68359375, 0)
+animsrblx.Size = UDim2.new(0, 102, 0, 27)
+animsrblx.Font = Enum.Font.SourceSans
+animsrblx.Text = "Roblox Animations"
+animsrblx.TextColor3 = Color3.fromRGB(0, 0, 0)
+animsrblx.TextSize = 14.000
+animsrblx.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://gitlab.com/Tsuniox/lua-stuff/-/raw/master/R15GUI.lua"))()
+end)
+
+ragdollgui.Name = "ragdoll gui"
+ragdollgui.Parent = main
+ragdollgui.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+ragdollgui.Position = UDim2.new(0.0850574747, 0, 0.68359375, 0)
+ragdollgui.Size = UDim2.new(0, 102, 0, 27)
+ragdollgui.Font = Enum.Font.SourceSans
+ragdollgui.Text = "Ragdoll engine GUI"
+ragdollgui.TextColor3 = Color3.fromRGB(0, 0, 0)
+ragdollgui.TextSize = 14.000
+ragdollgui.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/T7weKqag'),true))()
+end)
+
+animssynonly.Name = "anims syn only"
+animssynonly.Parent = main
+animssynonly.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
+animssynonly.Position = UDim2.new(0.381609201, 0, 0.682044983, 0)
+animssynonly.Size = UDim2.new(0, 102, 0, 27)
+animssynonly.Font = Enum.Font.SourceSans
+animssynonly.Text = "Animations syn x only"
+animssynonly.TextColor3 = Color3.fromRGB(0, 0, 0)
+animssynonly.TextSize = 14.000
+animssynonly.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/09PvJU1M", true))()
+end)
+
+noupdate.Name = "no update"
+noupdate.Parent = main
+noupdate.BackgroundColor3 = Color3.fromRGB(61, 61, 61)
+noupdate.BackgroundTransparency = 1.000
+noupdate.Position = UDim2.new(0, 0, 0.938326001, 0)
+noupdate.Size = UDim2.new(0, 93, 0, 14)
+noupdate.Font = Enum.Font.SourceSans
+noupdate.Text = "(Does not update)"
+noupdate.TextColor3 = Color3.fromRGB(0, 0, 0)
+noupdate.TextSize = 14.000
